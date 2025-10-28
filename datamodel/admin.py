01a502032eccce39ac472b7d9180df557a5f1e58
michu999 <mgoluch2020@gmail.com>
2025-10-28 18:54:10 +0100

    created a default django project boilerplate

--- datamodel/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import AIModelLog
-# Register your models here.
-
-admin.site.register(AIModelLog)
--- datamodel/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import AIModelLog
-
-class AIModelLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AIModelLog
-        fields = '__all__'
-        read_only_fields = ('id', 'timestamp', 'processing_time')
